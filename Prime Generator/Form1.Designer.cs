451cd8ce840564e1fd77ee7e5e303a85630c9cf1
nubington <nubington@yahoo.com>
2014-03-02 00:27:26 -0500

    first commit

---
+++ Prime Generator/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Prime_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxResults = new System.Windows.Forms.TextBox();
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtBoxProgress = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.checkBoxSave = new System.Windows.Forms.CheckBox();
+            this.txtBoxSave = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxResults
+            // 
+            this.txtBoxResults.Location = new System.Drawing.Point(7, 67);
+            this.txtBoxResults.Multiline = true;
+            this.txtBoxResults.Name = "txtBoxResults";
+            this.txtBoxResults.ReadOnly = true;
+            this.txtBoxResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBoxResults.Size = new System.Drawing.Size(260, 163);
+            this.txtBoxResults.TabIndex = 0;
+            this.txtBoxResults.TabStop = false;
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Location = new System.Drawing.Point(7, 12);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxInput.TabIndex = 0;
+            this.txtBoxInput.TextChanged += new System.EventHandler(this.txtBoxInput_TextChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Enabled = false;
+            this.btnGo.Location = new System.Drawing.Point(7, 38);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(47, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.TabStop = false;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(7, 236);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(219, 20);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(232, 38);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(35, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(176, 38);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(50, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtBoxProgress
+            // 
+            this.txtBoxProgress.Location = new System.Drawing.Point(232, 236);
+            this.txtBoxProgress.Name = "txtBoxProgress";
+            this.txtBoxProgress.ReadOnly = true;
+            this.txtBoxProgress.Size = new System.Drawing.Size(35, 20);
+            this.txtBoxProgress.TabIndex = 6;
+            this.txtBoxProgress.TabStop = false;
+            this.txtBoxProgress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(120, 38);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(50, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.TabStop = false;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Enabled = false;
+            this.btnResume.Location = new System.Drawing.Point(60, 38);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(54, 23);
+            this.btnResume.TabIndex = 8;
+            this.btnResume.TabStop = false;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // checkBoxSave
+            // 
+            this.checkBoxSave.AutoSize = true;
+            this.checkBoxSave.Location = new System.Drawing.Point(113, 14);
+            this.checkBoxSave.Name = "checkBoxSave";
+            this.checkBoxSave.Size = new System.Drawing.Size(66, 17);
+            this.checkBoxSave.TabIndex = 9;
+            this.checkBoxSave.TabStop = false;
+            this.checkBoxSave.Text = "Save to:";
+            this.checkBoxSave.UseVisualStyleBackColor = true;
+            this.checkBoxSave.CheckedChanged += new System.EventHandler(this.checkBoxSave_CheckedChanged);
+            // 
+            // txtBoxSave
+            // 
+            this.txtBoxSave.Enabled = false;
+            this.txtBoxSave.Location = new System.Drawing.Point(180, 12);
+            this.txtBoxSave.Name = "txtBoxSave";
+            this.txtBoxSave.Size = new System.Drawing.Size(82, 20);
+            this.txtBoxSave.TabIndex = 1;
+            this.txtBoxSave.TextChanged += new System.EventHandler(this.txtBoxSave_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(274, 261);
+            this.Controls.Add(this.txtBoxSave);
+            this.Controls.Add(this.checkBoxSave);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtBoxProgress);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtBoxInput);
+            this.Controls.Add(this.txtBoxResults);
+            this.MaximumSize = new System.Drawing.Size(290, 299);
+            this.MinimumSize = new System.Drawing.Size(290, 299);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prime Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxResults;
+        private System.Windows.Forms.TextBox txtBoxInput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtBoxProgress;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.CheckBox checkBoxSave;
+        private System.Windows.Forms.TextBox txtBoxSave;
+
+    }
+}
+
---
+++ Prime Generator/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace Prime_Generator
+{
+
+    public partial class Form1 : Form
+    {
+        private uint numberOfPrimes;
+        private Stopwatch stopWatch = new Stopwatch();
+        private Thread currentPrimeThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void txtBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            uint temp;
+            btnGo.Enabled = uint.TryParse(txtBoxInput.Text, out temp) && temp != 0 && (!checkBoxSave.Checked || txtBoxSave.Text.Length > 0);
+        }
+
+        private void txtBoxSave_TextChanged(object sender, EventArgs e)
+        {
+            txtBoxInput_TextChanged(sender, e);
+        }
+
+        private void checkBoxSave_CheckedChanged(object sender, EventArgs e)
+        {
+            txtBoxSave.Enabled = checkBoxSave.Checked;
+            txtBoxInput_TextChanged(sender, e);
+            //if (txtBoxSave.Text.Length == 0)
+            //    btnGo.Enabled = false;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            numberOfPrimes = uint.Parse(txtBoxInput.Text);
+            txtBoxResults.Clear();
+            btnGo.Enabled = false;
+            btnStop.Enabled = true;
+            btnCancel.Enabled = true;
+            txtBoxInput.ReadOnly = true;
+            progressBar.Value = 0;
+            txtBoxProgress.Text = "0%";
+
+            stopWatch.Restart();
+
+            currentPrimeThread = new Thread(new ThreadStart(calculatePrimes));
+            currentPrimeThread.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            currentPrimeThread.Suspend();
+            btnStop.Enabled = false;
+            btnResume.Enabled = true;
+            stopWatch.Stop();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            currentPrimeThread.Resume();
+            btnStop.Enabled = true;
+            btnResume.Enabled = false;
+            stopWatch.Start();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            currentPrimeThread.Suspend();
+            currentPrimeThread.Resume();
+            currentPrimeThread.Abort();
+
+            btnGo.Enabled = true;
+            btnStop.Enabled = false;
+            btnResume.Enabled = false;
+            btnCancel.Enabled = false;
+            txtBoxInput.ReadOnly = false;
+
+            stopWatch.Reset();
+
+            progressBar.Value = 0;
+            txtBoxProgress.Clear();
+            //this.Invoke((MethodInvoker)delegate { txtBoxResults.Clear(); });
+            this.Invoke((MethodInvoker)delegate { txtBoxResults.Text = ""; });
+        }
+
+        // prime thread delegates
+        delegate void PerformStepDelegate();
+        delegate void AppendTextDelegate(string s);
+        delegate void SetTxtBoxProgressText(string s);
+
+        // prime thread method
+        private void calculatePrimes()
+        {
+            PerformStepDelegate performStep = new PerformStepDelegate(progressBar.PerformStep);
+            //MethodInvoker performStep = delegate { progressBar.PerformStep(); };
+            AppendTextDelegate appendText = new AppendTextDelegate(txtBoxResults.AppendText);
+            SetTxtBoxProgressText setTxtBoxProgressText = delegate(string s) { txtBoxProgress.Text = s; };
+
+            uint count = 0;
+            uint stepValue = numberOfPrimes / 100;
+            if (stepValue == 0)
+                stepValue = 1;
+
+            this.Invoke(appendText, new object[] { 2 + "\r\n" });
+            if (++count == numberOfPrimes)
+                goto end;
+
+            for (ulong n = 3; ; n += 2)
+            {
+                if (isPrime(n))
+                {
+                    this.Invoke(appendText, new object[] { n + "\r\n" });
+                    if (++count == numberOfPrimes)
+                        break;
+                    if (count % stepValue == 0)
+                    {
+                        this.Invoke(performStep);
+                        //this.Invoke(setTxtBoxProgressText, new object[] { progressBar.Value + "%" });
+                        this.Invoke((MethodInvoker)delegate { txtBoxProgress.Text = progressBar.Value + "%"; });
+                    }
+                }
+            }
+
+            end:
+            this.Invoke((MethodInvoker)delegate { progressBar.Value = 100; });
+            this.Invoke((MethodInvoker)delegate { txtBoxProgress.Text = "100%"; });
+            this.Invoke((MethodInvoker)delegate { btnGo.Enabled = true; });
+            this.Invoke((MethodInvoker)delegate { btnStop.Enabled = false; });
+            this.Invoke((MethodInvoker)delegate { btnCancel.Enabled = false; });
+            this.Invoke((MethodInvoker)delegate { txtBoxInput.ReadOnly = false; });
+
+            stopWatch.Stop();
+
+            if (checkBoxSave.Checked)
+                File.WriteAllText(txtBoxSave.Text, txtBoxResults.Text, Encoding.Unicode);
+
+            CustomTimeSpan t = new CustomTimeSpan(stopWatch.ElapsedMilliseconds);
+            this.Invoke(appendText, new object[] { "\r\nCompleted in " + t });
+            //this.Invoke(appendText, new object[] { "\r\nCompleted in " + (stopWatch.ElapsedMilliseconds / 1000.0) + " seconds." });
+        }
+
+        public static bool isPrime(ulong n)
+        {
+            for (ulong i = 2; i * i <= n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+
+    class CustomTimeSpan
+    {
+        private long hours, minutes, milliseconds;
+        private double seconds;
+
+        public CustomTimeSpan()
+        {
+            hours = minutes = 0;
+            seconds = 0.0;
+        }
+        public CustomTimeSpan(long ms)
+        {
+            hours = ms / (1000 * 60 * 60);
+            ms = ms % (1000 * 60 * 60);
+            minutes = ms / (1000 * 60);
+            ms = ms % (1000 * 60);
+            seconds = ms / 1000.0;
+            milliseconds = ms;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("");
+            if (hours > 0)
+                result.Append(hours + (hours > 1 ? " hours, " : " hour, "));
+            if (minutes > 0)
+                result.Append(minutes + (minutes > 1 ? " minutes, " : " minute, "));
+            if (seconds >= 1)
+                result.Append(seconds + " seconds.");
+            else
+                result.Append(milliseconds + " milliseconds.");
+            return result.ToString();
+        }
+    }
+}
